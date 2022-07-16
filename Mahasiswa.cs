4f0d9a2a0635ad055a7ee01a3cd03805f33001b4
alexfredys <101544761+alexfredys@users.noreply.github.com>
2022-07-16 10:15:01 +0700

    Create Mahasiswa.cs

---
+++ Mahasiswa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        // PERINTAH: lengkapi property class mahasiswa, sesuai petunjuk soal
+
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public string Jenis_kelamin { get; set; }
+        public float Ipk { get; set; }
+
+    }
+}
